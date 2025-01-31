ac2d1aaa331d30b6ccd9bb39a6b81b325d68f03c
Iwona Grabska-Gradzińska <grabska@gmail.com>
2025-01-31 04:11:53 +0100

    fix pagination

--- cypress/e2e/people.cy.js
+++ cypress/e2e/people.cy.js
@@ -3,14 +3,21 @@ describe('people page', () => {
         cy.visit('people.html?search=&category=Alle')
     })
 
+    it('Check 404', () => {
+        cy.url().should('include', '/people.html?search=&category=Alle'); 
+        cy.document().its('readyState').should('eq', 'complete'); 
+      });
 
-    it('displays list of people', () => {
-        cy.get('pb-split-list > :nth-child(1) > a').should('be.visible')
+      it('Check main metadate', () => {
+        cy.title().should('not.be.empty'); 
+        // cy.get('meta[name="description"]').should('have.attr', 'content').and('not.be.empty');
+      });
+
+    it('list of people is not empty', () => {
+        cy.get('span.register-item').its('length').should('be.gte', 0)
     })
 
-    it('there exist Abletten', () => {
-        // cy.get('pb-split-list > :nth-child(11) > a').contains('Abletten')
-        // .should('have.attr', 'href', 'detail.html?ref=per026637')
+    it('there exist person of name Abletten', () => {
         cy.contains('a','Abletten')
         .should('exist') 
         .and('be.visible') 
@@ -18,8 +25,16 @@ describe('people page', () => {
        .click(); 
        cy.wait(10)
 
-       // Check if exists "Abletten" in new page
+       // Check if exists headline "Abletten" in new page
        cy.contains('h2', 'Johannes Abletten').should('be.visible')
        .should('have.class', 'tei-person1');
     })
+
+    it('Serch results for “Böp” is equal 1', () => {
+        cy.get('input[name="search"]').first().focus()
+        .type('Böp{enter}') 
+        .wait(1000)
+        cy.get('.register-item').should('be.visible')
+        .should('have.length', 1);
+      });
 })
