1dcb0e9e7dd4a3dcb11be367d601f55fdc831409
duncdrum <d.paterson@me.com>
2025-02-15 14:10:31 +0100

    refactor(tests): minor fixes

--- cypress/e2e/register-places.cy.js
+++ cypress/e2e/register-places.cy.js
@@ -1,42 +1,49 @@
 describe('Places register check', () => {
   beforeEach('loads', () => {
       cy.visit('places.html?search=&category=Alle')
-      .wait(1000)
   })
 
   it('Check meta title', () => {
-    cy.title().should('not.be.empty');
+    cy.title()
+      .should('not.be.empty')
   });
 
   it.skip('Check meta description', () => {
-    cy.get('meta[name="description"]').should('have.attr', 'content').and('not.be.empty');
+    cy.get('meta[name="description"]')
+      .should('have.attr', 'content')
+      .and('not.be.empty')
   });
 
   it('list of places is not empty', () => {
-      cy.get('span.register-item').its('length').should('be.gte', 0)
+      cy.get('span.register-item')
+        .its('length')
+        .should('be.gte', 0)
   })
 
   
   it('Check if there exist place of name Aare and content of detail page', () => {
-      cy.contains('a', 'Aare')
-          .should('exist')
-          .and('be.visible')
-          .and('have.attr', 'href', 'detail.html?ref=loc009226')
-          .click();
-      cy.wait(10)
-
+      cy.get('pb-split-list')
+        .contains('a', 'Aare')
+        .should('exist')
+        .and('be.visible')
+        .and('have.attr', 'href', 'detail.html?ref=loc009226')
+        .click()
+      cy.url()
+        .should('contain', '009226')
       // Check if exists headline "Aare" in new page
-      cy.contains('h2', 'Aare').should('be.visible')
-          .should('have.class', 'tei-place1');
-          cy.wait(10)
+      cy.contains('h2', 'Aare')
+        .should('be.visible')
+        .should('have.class', 'tei-place1')
   })
 
   it('Serch results for Allenried” is equal 1', () => {
-      cy.get('input[name="search"]').first().focus()
-          .type('Allenried{enter}')
-          .wait(1000)
-      .get('.register-item').should('be.visible')
-          .should('have.length', 1);
+      cy.get('input[name="search"]')
+        .first()
+        .focus()
+        .type('Allenried{enter}')
+      cy.get('.register-item')
+        .should('be.visible')
+        .should('have.length', 1)
   });
 
 
